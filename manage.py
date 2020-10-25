3604b0cdc7a2daa0cbd0640948d4be1807bc79ec
Munge Ryan <mungeryan@gmail.com>
2020-10-25 18:16:03 +0300

    changing create_app to production

--- manage.py
+++ manage.py
@@ -1,6 +1,6 @@
 from flask_script import Manager,Server
 from app import create_app,db
-from app.models import User
+from app.models import User,Pitchy,Likes,Dislikes,Comment
 from flask_migrate import Migrate,MigrateCommand
 
 app = create_app('production')
@@ -13,7 +13,7 @@ manager.add_command('run',Server)
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User)
+    return dict(app = app,db = db,User = User,Pitchy = Pitchy,Likes = Likes,Dislikes = Dislikes,Comment = Comment)
 
 @manager.command
 def test():
