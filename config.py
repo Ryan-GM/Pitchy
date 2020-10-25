01d9124b9ca1eaeb4a2ee499935b1a6335134235
Munge Ryan <mungeryan@gmail.com>
2020-10-25 18:14:20 +0300

    configuring test for pitchy

--- config.py
+++ config.py
@@ -1,20 +1,23 @@
 import os
 
 class Config:
-   SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLACHEMY_TRACK_MODIFICATIONS = True
+    SECRET_KEY = os.environ.get('PLUMMY')
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
 
 class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy'
     DEBUG = True
 
-# class TestConfig(Config):
-#     SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy_test'
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy_test'
 
 config_options = {
     'development':DevConfig,
     'production':ProdConfig,
-    # 'test':TestConfig
+    'test':TestConfig
 }
 
