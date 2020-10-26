c11c832bbc23a450387f8907a776a3b9c337622e
Munge Ryan <mungeryan@gmail.com>
2020-10-26 12:40:24 +0300

    updateprofile class

---
+++ app/main/form.py
@@ -0,0 +1,17 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField,TextAreaField,SubmitField,SelectField
+from wtforms.validators import Required
+
+class PitchyForm(FlaskForm):
+    title = StringField('Title',validators=[Required()])
+    post = TextAreaField('Pitch it', validators=[Required])
+    category = SelectField('Category',choices=[('Cheesy','Cheesy'),('Life','Life','Funny','Funny')],validators=[Required()])
+    submit = SubmitField('Pitchy')
+
+class CommentForm(FlaskForm):
+    comment = TextAreaField('Do leave a comment',validators=[Required()])
+    submit = SubmitField('Comment')
+
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('A brief one about you',validators=[Required()])
+    submit = SubmitField('Save')
