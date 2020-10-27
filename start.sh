ffb3dfdb87929f33d62cad64c1af8f6340fde3f1
Munge Ryan <mungeryan@gmail.com>
2020-10-27 12:22:39 +0300

    exporting secret key

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 export MAIL_USERNAME = monger@gmail.com
 export MAIL_PASSWORD = killmonger
 export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy'
-export SECRET_KEY = PLUMMY
+export SECRET_KEY = Flask WTF Secret Key
 
 python manage.py server
