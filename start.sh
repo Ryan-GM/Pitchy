ff221b7ac429d83e8ba0f48adbbdf8943c301d88
Munge Ryan <mungeryan@gmail.com>
2020-10-27 08:33:17 +0300

    exporting mail username

---
+++ start.sh
@@ -0,0 +1,6 @@
+export MAIL_USERNAME = monger@gmail.com
+export MAIL_PASSWORD = killmonger
+export SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy'
+export SECRET_KEY = PLUMMY
+
+python manage.py server
