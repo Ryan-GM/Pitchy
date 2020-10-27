db0dd7c9a8c4f97c86be277d305b79642a1fe283
Munge Ryan <mungeryan@gmail.com>
2020-10-27 08:22:08 +0300

    configuring emails

--- config.py
+++ config.py
@@ -1,9 +1,19 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:slowwhine@localhost/pitchy'
     SQLACHEMY_TRACK_MODIFICATIONS = True
     SECRET_KEY = os.environ.get('PLUMMY')
+    #simple mde configurations
+    SIMPLEMDE_JS_IIFE = True
+    SIMPLEMDE_USE_CDN = True
+    UPLOADED_PHOTOS_DEST = 'app/static/photos'
+    #email configurations
+    MAIL_SERVER = 'smtp.googleemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
 
 class ProdConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
